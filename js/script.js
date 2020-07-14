a62363803fd181ec3d17031be19271d13778d848
mohdfahoum <mohd.fahoum@gmail.com>
2020-07-14 14:37:01 +0300

    adding Table

--- js/script.js
+++ js/script.js
@@ -1,90 +1,100 @@
 'use_strict';
 
-//['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'],
-var hours = ['6:00 am', '7:00 am', '8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm', '1:00 pm', '2:00 pm', '3:00 pm', '4:00 pm', '5:00 pm', '6:00 pm', '7:00 pm'];
 
+var Hours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Daily Location Total'];
 
-function BranchLocation(location, maxCustomers, minCustomers, avgCookies, workingHours, salesOfToDay, salesArray) {
-    var salesArray = [];
-    salesArray.push(salesOfToDay);
-
-
+function BranchLocation(location, minCustomer, maxCustomer, avgCookies, totalSales, salesArray) {
     this.location = location;
-    this.maxCustomers = maxCustomers;
-    this.minCustomers = minCustomers;
+    this.minCustomer = minCustomer;
+    this.maxCustomer = maxCustomer;
     this.avgCookies = avgCookies;
-    this.workingHours = workingHours;// will be defined as array in the object 
-    this.salesOfToDay = salesOfToDay; //first 
-    this.salesArray = salesArray; // array pushed in line 9
-
-    
+    this.totalSales = totalSales;
+    this.salesArray = salesArray;
 }
-
-
-BranchLocation.prototype.cookiesAmount = function (maxCustomers, minCustomers) {
-
-    for (let index = 0; index < this.workingHours.length - 1; index++) {
-
-        var numOfSales = Math.round((Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers) * this.avgCookies);
-        this.salesOfToDay += numOfSales;
+BranchLocation.prototype.CookiesAmount = function () {
+    for (var index = 0; index < Hours.length - 1; index++) {
+        var numOfSales = Math.round((Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer) * this.avgCookies);
+        this.totalSales += numOfSales;
         this.salesArray.push(numOfSales);
 
+    }
+}
+var Seattle = new BranchLocation('Seattle', 23, 65, 6.3, 0, []);
+console.log(Seattle);
+var Tokyo = new BranchLocation('Tokyo', 3, 24, 1.2, 0, []);
+console.log(Tokyo);
+var Dubai = new BranchLocation('Dubai', 11, 38, 3.7, 0, []);
+console.log(Dubai);
+var Paris = new BranchLocation('Paris', 20, 38, 2.3, 0, []);
+console.log(Paris);
+var Lima = new BranchLocation('Lima', 2, 16, 4.6, 0, []);
+console.log(Lima);
+var locations = [Seattle, Tokyo, Dubai, Paris, Lima];
 
-    } console.log(this.salesOfToDay);
-    console.log(this.salesArray);
-
+for (let i = 0; i < locations.length; i++) {
+    locations[i].CookiesAmount();
 }
 
-BranchLocation.prototype.render = function () {
-    var parentElement = document.getElementById('salesTabel');
+BranchLocation.render = function () {
 
-    var shopLocation = document.createElement('p');
-    shopLocation.innerHTML = this.location;
-    parentElement.appendChild(shopLocation);
+    var parentElement = document.getElementById('salmonTable');
+    var table = document.createElement('table');
+    table.setAttribute('border','table-style');
 
+    //////////////////////////
+    //////TABLE///////////////
+    //////////////////////////
+    
+    var tr = document.createElement('tr');
+    var th1 = document.createElement('th');
+    tr.appendChild(th1);
+    for (var index = 0; index < Hours.length; index++) {
+        var th = document.createElement('th');
+        th.innerHTML = Hours[index];
+        tr.appendChild(th);
+        table.appendChild(tr);
 
-
-    var ul = document.createElement('ul');
-    parentElement.appendChild(ul);
-    for (let index = 0; index < this.workingHours.length; index++) {
-        var li = document.createElement('li');
-        li.innerHTML = this.workingHours[index];
-        ul.appendChild(li);
-
-    if (this.workingHours[index] == 'Total') {
-        var span = document.createElement('span');
-        span.innerHTML = (' :  ' + this.salesOfToDay);
-        li.appendChild(span);
-    } else {
-
-        var span = document.createElement('span');
-        span.innerHTML = ('  :  ' + this.salesArray[index] + ' cookies');
-        li.appendChild(span);
     }
 
+    for (var i = 0; i < locations.length; i++) {
+        var tr2 = document.createElement('tr');
+        tr2.innerHTML = locations[i].location;
+        tr2.setAttribute('class', 'location');
+        console.log(locations[i].location);
+
+        for (var index = 0; index < Hours.length; index++) {
+            var td = document.createElement('td');
+            if (index === Hours.length - 1) {
+                td.innerHTML = locations[i].totalSales;
+            } else {
+                td.innerHTML = locations[i].salesArray[index];
+
+            }
+            tr2.appendChild(td);
+            table.appendChild(tr2);
+        }
+    }
+    var tr3 = document.createElement('tr');
+    tr3.innerHTML = 'Totals';
+
+    for (var i = 0; i < Hours.length; i++) {
+        var td = document.createElement('td');
+        var timeFrame = 0;
+
+        for (var y = 0; y < locations.length; y++) {
+            if (i === Hours.length - 1) {
+                timeFrame += locations[y].totalSales;
+            } else {
+                timeFrame += locations[y].salesArray[i];
+            }
+        }
+        td.innerText = timeFrame;
+        tr3.appendChild(td);
+    }
+    table.appendChild(tr3);
+
+    parentElement.appendChild(table);
 }
 
-}
-
-var Tokyo = new BranchLocation('Tokyo', '15', '4', '4.2', ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'], 44);
-var Seattle = new BranchLocation('Seattle', '12', '6', '1.2', ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'], 50);
-var Lima = new BranchLocation('Lima', '20', '2', '0.4', ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'], 63)
-var Dubai = new BranchLocation('Dubai', '40', '8', '3.4', ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'], 80)
-var Paris = new BranchLocation('Paris', '14', '3', '1.4', ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00am', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm', 'Total'], 73)
-
-Tokyo.cookiesAmount();
-Tokyo.render();
-
-Seattle.cookiesAmount();
-Seattle.render();
-
-Lima.cookiesAmount();
-Lima.render();
-
-Dubai.cookiesAmount();
-Dubai.render();
-
-Paris.cookiesAmount();
-Paris.render();
-
-
+console.log(locations[0].salesArray);
+BranchLocation.render();
